9d9dc857035afb505be92ac3d0db206a93778d85
Michael Frajman <michael.frajman@mail.mcgill.ca>
2024-04-18 22:22:16 -0400

    final cleanup

--- js/a3.js
+++ js/a3.js
@@ -1,6 +1,7 @@
 // mapbox token
 mapboxgl.accessToken = "pk.eyJ1IjoibWljaGFlbGZyYWptYW4xIiwiYSI6ImNsdXZzaTNiNjA2ejQycXBiaGU5dDNxM2UifQ.xQkObwg-QzODO9fKFMGUTw"
 
+// generate map and have it centered to display Staten Island
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
@@ -8,6 +9,7 @@ const map = new mapboxgl.Map({
     zoom: 11
 });
 
+// Based on Chris Wong's pizza map example
 map.addControl(new mapboxgl.NavigationControl());
 
 // loop over the station array to make a marker for each record
@@ -17,8 +19,9 @@ stations.forEach(function (stationRecord) {
     const popup = new mapboxgl.Popup({
         offset: 24,
         anchor: 'bottom'
+    // display the text in the popup
     }).setText(
-        `${stationRecord.name}`
+        `${stationRecord.StopName}`
     );
 
     // create a marker, set the coordinates, add the popup, add it to the map
